c9cebfd9798c235bfb315fe7437b29408486a456
SpherionOS <122868668+SpherionOS@users.noreply.github.com>
2024-04-06 01:13:26 +1000

    Update run_bot.sh
    Hardcoded it to use version 0.3.9

--- run_bot.sh
+++ run_bot.sh
@@ -2,10 +2,10 @@
 
 echo "Starting JMusicBot"
 
-VER=$(curl --silent "https://api.github.com/repos/jagrosh/MusicBot/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+#VER=$(curl --silent "https://api.github.com/repos/jagrosh/MusicBot/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 
-if [ ! -f JMusicBot-$VER.jar ]; then	
-  wget https://github.com/jagrosh/MusicBot/releases/download/$VER/JMusicBot-$VER.jar
-fi
+#if [ ! -f JMusicBot-$VER.jar ]; then	
+wget https://github.com/jagrosh/MusicBot/releases/download/0.3.9/JMusicBot-0.3.9.jar
+#fi
 
-java -Dnogui=true -Dconfig=/config/config.txt -jar JMusicBot-$VER.jar 
+java -Dnogui=true -Dconfig=/config/config.txt -jar JMusicBot-0.3.9.jar 
